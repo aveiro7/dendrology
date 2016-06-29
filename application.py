fe30e931b356c8224f2ba088213b3f05c4da2925
aveiro <o.rosik.rosinska@gmail.com>
2016-06-29 15:04:47 +0200

    the main page

--- application.py
+++ application.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+@app.route('/')
+def show_tree():
+    return render_template('show_tree.html')
 
 if __name__ == "__main__":
     app.run()
