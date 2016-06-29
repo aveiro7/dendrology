f564d8727160f5f47b24abb9e38198afbcf3a363
aveiro <o.rosik.rosinska@gmail.com>
2016-06-29 15:30:01 +0200

    modified layout

--- application.py
+++ application.py
@@ -7,5 +7,17 @@ app.config.from_object(__name__)
 def show_tree():
     return render_template('show_tree.html')
 
+@app.route('/register')
+def register():
+    return "register"
+
+@app.route('/login')
+def login():
+    return "login"
+
+@app.route('/logout')
+def logout():
+    return "logout"
+
 if __name__ == "__main__":
     app.run()
