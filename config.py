9f0bb5335c9f5da12b7f4e31176d7f21f602c13f
aveiro <o.rosik.rosinska@gmail.com>
2016-07-05 22:20:16 +0200

    config.py refactoring

--- config.py
+++ config.py
@@ -1,32 +1,35 @@
 import sqlite3
+from application import get_db
 
-def set_config(app):
-
-    db = sqlite3.connect(app.config['DATABASE'])
-    db.row_factory = sqlite3.Row
+def set_mail_config(app):
+    db = get_db()
 
     query = '''select value
             from config
             where key = ?'''
-    cur = db.execute(query, ["secret_key"])
-    secret_key = cur.fetchone()[0]
-
-    cur = db.execute(query, ["mail_server"])
-    mail_server = cur.fetchone()[0]
-
-    cur = db.execute(query, ["mail_username"])
-    mail_username = cur.fetchone()[0]
-
-    cur = db.execute(query, ["mail_password"])
-    mail_password = cur.fetchone()[0]
-
+    mail_server = db.execute(query, ["mail_server"]).fetchone()[0]
+    mail_username = db.execute(query, ["mail_username"]).fetchone()[0]
+    mail_password = db.execute(query, ["mail_password"]).fetchone()[0]
+    
     app.config.update(dict(
-        SECRET_KEY=secret_key,
-        MAIL_SERVER = mail_server,
-        MAIL_PORT = 587,
-        MAIL_USE_TLS = True,
-        MAIL_USE_SSL = False,
-        MAIL_USERNAME = mail_username,
-        MAIL_PASSWORD = mail_password
+        MAIL_SERVER=mail_server,
+        MAIL_PORT=587,
+        MAIL_USE_TLS=True,
+        MAIL_USE_SSL=False,
+        MAIL_USERNAME=mail_username,
+        MAIL_PASSWORD=mail_password
         ))
 
+def set_cookie_config(app):
+    db = get_db()
+
+    query = '''select value
+            from config
+            where key = ?'''
+    secret_key = db.execute(query, ["secret_key"]).fetchone()[0]
+
+    app.config['SECRET_KEY'] = secret_key
+
+def set_config(app):
+    set_cookie_config(app)
+    set_mail_config(app)
